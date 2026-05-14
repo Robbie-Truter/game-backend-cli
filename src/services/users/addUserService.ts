c3878bf8c403eaa535f43cbbe16b8b3d9c2fadc5
Robbie-Truter <timothylang6@gmail.com>
2026-05-14 23:07:52 +0200

    refactor: remove redundant refreshToken field from User model and improve error handling in addUserService

--- src/services/users/addUserService.ts
+++ src/services/users/addUserService.ts
@@ -5,21 +5,22 @@ import type { TAddUser } from '../../types/users/addUserSchema.js';
 
 export async function addUserService(payload: TAddUser) {
   try {
+    console.log('Attempting to create user with payload:', payload);
     const newUser = await prisma.user.create({
       data: payload,
     });
 
     return newUser;
   } catch (error) {
-    if (error instanceof Prisma.PrismaClientKnownRequestError) {
-      // P2002 is the error code for a unique constraint violation
-      if (error) {
-        const err: AppError = new Error(
-          `User with this username or email already exists.`,
-        );
-        err.status = 409;
-        throw err;
-      }
+    if (
+      error instanceof Prisma.PrismaClientKnownRequestError &&
+      error.code === 'P2002'
+    ) {
+      const err: AppError = new Error(
+        `User with this username or email already exists.`,
+      );
+      err.status = 409;
+      throw err;
     }
     // For other errors, we re-throw them
     throw error;
