b3c2738161e216fbd1f61926731ed8b63563a3dd
Robbie-Truter <timothylang6@gmail.com>
2026-05-17 20:43:57 +0200

    feat: implement logout controller and add POST /logout route

--- src/routes/authRoutes.ts
+++ src/routes/authRoutes.ts
@@ -5,11 +5,13 @@ import { LoginSchema } from '../types/auth/loginSchema.js';
 import registerController from '../controllers/auth/registerController.js';
 import refreshTokenController from '../controllers/auth/refreshTokenController.js';
 import loginController from '../controllers/auth/loginController.js';
+import logoutController from '../controllers/auth/logoutController.js';
 
 const router = express.Router();
 
 // POST
 router.post('/login', validationMiddleware(LoginSchema), loginController);
+router.post('/logout', logoutController);
 router.post(
   '/register',
   validationMiddleware(RegisterSchema),
